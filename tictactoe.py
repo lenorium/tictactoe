4d93d4f643a6a6af4c773f529f25a628b89ba3d2
Rimma Iakhina <lenorium>
2022-08-10 16:28:34 +0200

    переход на классы

--- tictactoe.py
+++ tictactoe.py
@@ -8,86 +8,99 @@ import random
 X_SYMBOL = 'X'
 O_SYMBOL = 'O'
 EMPTY_CELL = ' '
-GRID_SIZE = 3
-TOTAL = GRID_SIZE ** 2
 
 
-def create_grid():
-    # так делать не надо, потому что он создает вложенный список,
-    # где элементами являются не несколько разных списков,
-    # а один и тот же, просто ссылки повторяются
-    # типа: [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-    # но id() каждого из этих трех внутренних списков один и тот же
-    #  ---> return [[EMPTY_CELL] * GRID_SIZE] * GRID_SIZE
-    initial_state = [EMPTY_CELL] * TOTAL
-    return [initial_state[i: i + GRID_SIZE] for i in range(0, TOTAL, GRID_SIZE)]
+class Game:
+    def __init__(self, grid):
+        self.grid = grid
+
+    def is_finished(self):
+        winner = self.get_winner()
+        if winner is not None:
+            print(f'{winner} wins')
+            return True
+
+        filled_cells = [s for line in self.grid.state for s in line if s != EMPTY_CELL]
+        if len(filled_cells) == self.grid.total_size:
+            print('Draw')
+            return True
+
+        return False
+
+    def get_winner(self):
+        players = {X_SYMBOL, O_SYMBOL}
+        for symbol in players:
+            if any(all(s == symbol for s in line) for line in self.grid.state) \
+                    or any(all(self.grid.state[j][i] == symbol
+                               for j in range(self.grid.size)) for i in range(self.grid.size)) \
+                    or all(self.grid.state[i][i] == symbol for i in range(self.grid.size)) \
+                    or all(self.grid.state[i][self.grid.size - 1 - i] == symbol for i in range(self.grid.size)):
+                return symbol
+        return None
 
 
-def print_grid(grid: list):
-    print('---------')
-    [print('|', *line, '|') for line in grid]
-    print('---------')
+class Grid:
+    side = 3
+    total_size = side ** 2
+
+    def __init__(self):
+        init_state = [EMPTY_CELL] * self.total_size
+        self.state = [init_state[i: i + self.side] for i in range(0, self.total_size, self.side)]
+
+    def print(self):
+        print('---------')
+        [print('|', *line, '|') for line in self.state]
+        print('---------')
 
 
-def is_finished(grid: list):
-    winner = get_winner(grid)
-    if winner is not None:
-        print(f'{winner} wins')
-        return True
+class Player:
+    def __init__(self, level, symbol):
+        self.level = level
+        self.symbol = symbol
+        self.is_human = level == 'user'
 
-    filled_cells = [s for line in grid for s in line if s != EMPTY_CELL]
-    if len(filled_cells) == TOTAL:
-        print('Draw')
-        return True
+    def make_move(self, grid: Grid):
+        if self.is_human:
+            while True:
+                coord = input('Enter the coordinates:')
+                try:
+                    i, j = [int(c) - 1 for c in coord.split()]
+                except ValueError:
+                    print('You should enter numbers!')
+                    continue
 
-    return False
+                if 0 > i or i > grid.side - 1 or 0 > j or j > grid.side - 1:
+                    print(f'Coordinates should be from 1 to {grid.side}!')
+                    continue
+
+                if grid.state[i][j] != EMPTY_CELL:
+                    print('This cell is occupied! Choose another one!')
+                    continue
+                break
+        else:
+            print(f'Making move level "{self.level}"')
+            empty_coordinates = [[i, j] for i in range(grid.side)
+                                 for j in range(grid.side) if grid.state[i][j] == EMPTY_CELL]
+            i, j = random.choice(empty_coordinates)
+
+        grid.state[i][j] = self.symbol
 
 
-def get_winner(grid: list):
-    players = {X_SYMBOL, O_SYMBOL}
-    for symbol in players:
-        if any(all(s == symbol for s in line) for line in grid) \
-                or any(all(grid[j][i] == symbol for j in range(GRID_SIZE)) for i in range(GRID_SIZE))\
-                or all(grid[i][i] == symbol for i in range(GRID_SIZE)) \
-                or all(grid[i][GRID_SIZE - 1 - i] == symbol for i in range(GRID_SIZE)):
-            return symbol
-    return None
+def play():
+    grid = Grid()
+    grid.print()
 
+    game = Game(grid)
 
-def make_move(grid: list, is_human: bool):
-    if is_human:
-        while True:
-            coord = input('Enter the coordinates:')
-            try:
-                i, j = [int(c) - 1 for c in coord.split()]
-            except ValueError:
-                print('You should enter numbers!')
-                continue
+    player1 = Player('user', X_SYMBOL)
+    player2 = Player('easy', O_SYMBOL)
 
-            if 0 > i or i > GRID_SIZE - 1 or 0 > j or j > GRID_SIZE - 1:
-                print(f'Coordinates should be from 1 to {GRID_SIZE}!')
-                continue
-
-            if grid[i][j] != EMPTY_CELL:
-                print('This cell is occupied! Choose another one!')
-                continue
-            break
-    else:
-        print('Making move level "easy"')
-        empty_coordinates = [[i, j] for i in range(GRID_SIZE) for j in range(GRID_SIZE) if grid[i][j] == EMPTY_CELL]
-        i, j = random.choice(empty_coordinates)
-
-    grid[i][j] = X_SYMBOL if is_human else O_SYMBOL
+    current_player = None
+    while not game.is_finished():
+        current_player = player2 if current_player == player1 else player1
+        current_player.make_move(grid)
+        grid.print()
 
 
 if __name__ == '__main__':
-    game_grid = create_grid()
-    print_grid(game_grid)
-    is_human = True
-
-    while True:
-        make_move(game_grid, is_human)
-        print_grid(game_grid)
-        if is_finished(game_grid):
-            break
-        is_human = not is_human
+    play()
