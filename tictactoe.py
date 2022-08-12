6d0ec50c27b375fd33fc962582bc27d8e3a741a8
lenorium <lenorium>
2022-08-12 13:44:14 +0200

    правки пр

--- tictactoe.py
+++ tictactoe.py
@@ -7,15 +7,8 @@ O_SYMBOL = 'O'
 GRID_SIZE = 3
 
 if __name__ == '__main__':
-    # grid = Grid(GRID_SIZE)
-    #
-    # player1 = Player('user', X_SYMBOL)
-    # player2 = Player('easy', O_SYMBOL)
-    #
-    # game = Game(grid, player1, player2)
-    # game.run()
-
     error = 'Bad parameters!'
+    game_levels = {'user', 'easy'}
     while True:
         try:
             command, *user_levels = input('Input command:').lower().strip().split()
@@ -26,22 +19,19 @@ if __name__ == '__main__':
         if command == 'exit':
             break
 
-        game_levels = {'user', 'easy'}
         if command == 'start':
             if len(user_levels) != 2 or any(level not in game_levels for level in user_levels):
                 print(error)
                 continue
 
-            level1, level2 = user_levels
-
             grid = Grid(GRID_SIZE)
 
-            player1 = Player(level1, X_SYMBOL)
-            player2 = Player(level2, O_SYMBOL)
+            player1 = Player(user_levels[0], X_SYMBOL)
+            player2 = Player(user_levels[1], O_SYMBOL)
 
             game = Game(grid, player1, player2)
             game.run()
 
         else:
-            break
+            print(error)
 
